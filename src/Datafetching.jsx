cb15ef265ac860c0c3e475b1f6144253a1e8ac10
Vigneshs19 <svignesh19042000@gmail.com>
2023-08-10 10:55:23 +0530

    first commit

---
+++ src/Datafetching.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Datafetching() {
+ const [data, setData] = useState([]);
+ const [loading,setloading]=useState(true);
+ const [error,seterror]=useState(error);
+
+
+  return (
+    <div>Datafetching</div>
+  )
+}
+
+export default Datafetching
